43360092901c8859c8365d2da163899f25a5fa1b
sasca37 <sasca37@naver.com>
2021-09-21 19:16:57 +0900

    repository.save 오류 해결

--- src/main/java/com/crawler/service/posts/PostsService.java
+++ src/main/java/com/crawler/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.crawler.service.posts;
 
+import com.crawler.domain.posts.Posts;
 import com.crawler.domain.posts.PostsRepository;
+import com.crawler.web.dto.PostsResponseDto;
 import com.crawler.web.dto.PostsSaveRequestDto;
+import com.crawler.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,22 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
-//        return postsRepository.save(requestDto.toEntity().getId());
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
