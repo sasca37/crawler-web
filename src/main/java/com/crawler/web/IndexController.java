d69721fe753611d221cf544f3f8f81405b88c2d7
sasca37 <sasca37@naver.com>
2021-09-21 19:41:47 +0900

    layout 설정

---
+++ src/main/java/com/crawler/web/IndexController.java
@@ -0,0 +1,31 @@
+package com.crawler.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+//    @GetMapping("/posts/update/{id}")
+//    public String postsUpdate(@PathVariable Long id, Model model) {
+//        PostsResponseDto dto = postsService.findById(id);
+//        model.addAttribute("post", dto);
+//
+//        return "posts-update";
+//    }
+}
